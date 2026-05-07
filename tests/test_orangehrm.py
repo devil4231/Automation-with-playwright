5869f97bedbf00b208e7bdbaafd19f41cc7a1016
devil4231 <118094282+devil4231@users.noreply.github.com>
2026-05-07 16:53:50 +0530

    Adding config, gitignore and some other basics to framework

--- tests/test_orangehrm.py
+++ tests/test_orangehrm.py
@@ -1,4 +1,5 @@
 import re
+from config import BASE_URL
 from playwright.sync_api import Page, expect
 from pages.orangehrm_login_page import LoginPage
 from pages.orangehrm_homepage import HomePage
@@ -8,7 +9,7 @@ def test_orangehrm(page):
     login_page = LoginPage(page)
     home_page = HomePage(page)
 
-    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    page.goto(BASE_URL)
 
     login_page.enter_username("Admin")
     login_page.enter_password("admin123")
